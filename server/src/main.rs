5c85f8c3f5ace76d681cae91575a8e60d1b5cb14
Qingyuan Liu <pixelledliu@gmail.com>
2020-12-19 23:11:20 -0800

    Set simple websocket server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::{Duration, Instant};
+
+use actix::prelude::*;
+use actix_files as fs;
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web_actors::ws;
+
+struct WsSession {
+    hb: Instant,
+}
+
+impl Actor for WsSession {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WsSession {
+    fn handle(
+        &mut self,
+        msg: Result<ws::Message, ws::ProtocolError>,
+        ctx: &mut Self::Context,
+    ) {
+        match msg {
+            _ => (),
+        }
+    }
+}
+
+async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let res = ws::start(WsSession {
+        hb: Instant::now(),
+    }, &req, stream);
+    println!("{:?}", res);
+    res
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web::resource("/").route(web::get().to(index)))
+            .service(fs::Files::new("/", "static/").index_file("index.html"))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
