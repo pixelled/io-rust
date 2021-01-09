459873569c65d6904d5d8e91d79a6e7d174f23c8
Arthur Pan <pansicheng@berkeley.edu>
2021-01-09 22:25:30 +0800

    Broken parameters.

--- server/src/system.rs
+++ server/src/system.rs
@@ -1,7 +1,7 @@
 use crate::component::*;
 use crate::event::{ChangeMovement, CreatePlayer, RemovePlayer};
 use crate::server::GameServer;
-use crate::View;
+use crate::{View, TICK_TIME};
 use bevy::app::{EventReader, Events};
 use bevy::ecs::{Command, Commands, Entity, Local, Or, Query, Res, ResMut, Resources, With, World};
 use game_shared::{
@@ -10,11 +10,11 @@ use game_shared::{
 };
 use rand::Rng;
 
-use bevy_rapier2d::na::Point2;
+use bevy_rapier2d::na::{Point2, Isometry, Translation, Rotation2};
 use bevy_rapier2d::physics::{RapierConfiguration, RigidBodyHandleComponent, EventQueue};
 use bevy_rapier2d::rapier::dynamics::{RigidBodyBuilder, RigidBodySet};
 use bevy_rapier2d::rapier::geometry::{ColliderBuilder, ContactEvent, ColliderSet};
-use bevy_rapier2d::rapier::ncollide::na::Vector2;
+use bevy_rapier2d::rapier::ncollide::na::{Vector2, Unit};
 
 use bevy::prelude::{Transform, Vec3};
 
@@ -22,16 +22,22 @@ const VIEW_X: f32 = 2080.0;
 const VIEW_Y: f32 = 1170.0;
 const INIT_MASS: f32 = 1.0;
 const INIT_RESTITUTION: f32 = 1.0;
-const CELESTIAL_MASS: f32 = 20000000.0;
+const CELESTIAL_MASS: f32 = 10000000.0;
 const GRAVITY_CONST: f32 = 1.0;
 
-fn create_entity(commands: &mut Commands, role: Role, rigid_body_builder: RigidBodyBuilder, collider_builder: ColliderBuilder) -> Entity {
+fn create_entity(commands: &mut Commands, role: Role, x: f32, y: f32, rigid_body_builder: RigidBodyBuilder, collider_builder: ColliderBuilder) -> Entity {
 	let entity = commands.spawn(()).current_entity().unwrap();
-	commands.insert(entity, (rigid_body_builder.user_data(entity.to_bits() as u128), collider_builder));
 	match role { Role::Player(name) => commands.insert_one(entity, Player { name }),
 		Role::Boundary(info) => commands.insert_one(entity, Boundary { info }),
 		Role::CelestialBody(form) => commands.insert_one(entity, CelestialBody { form }),
 		Role::Shape(id) => commands.insert_one(entity, Shape {id})};
+	commands.insert(entity, (
+			Thrust {x: 0.0, y: 0.0},
+			Transform::from_translation(Vec3::new(x, y, 0.0)),
+			rigid_body_builder.translation(x, y).user_data(entity.to_bits() as u128),
+			collider_builder
+		)
+	);
 	entity
 }
 
@@ -42,7 +48,7 @@ pub fn setup(commands: &mut Commands, mut configuration: ResMut<RapierConfigurat
 	configuration.gravity = Vector2::new(0.0, 0.0);
 
 	// Boundaries.
-	create_entity(commands, Role::Boundary("Top".to_string()),
+	create_entity(commands, Role::Boundary("Top".to_string()), 0.0, 0.0,
 				  RigidBodyBuilder::new_static(),
 				  ColliderBuilder::segment(Point2::new(0.0, 0.0), Point2::new(MAP_WIDTH, 0.0)).restitution(INIT_RESTITUTION));
 	// commands.spawn((
@@ -51,7 +57,7 @@ pub fn setup(commands: &mut Commands, mut configuration: ResMut<RapierConfigurat
 	// 	ColliderBuilder::segment(Point2::new(0.0, 0.0), Point2::new(MAP_WIDTH, 0.0))
 	// 		.restitution(INIT_RESTITUTION),
 	// ));
-	create_entity(commands, Role::Boundary("Left".to_string()),
+	create_entity(commands, Role::Boundary("Left".to_string()), 0.0, 0.0,
 				  RigidBodyBuilder::new_static(),
 				  ColliderBuilder::segment(Point2::new(0.0, 0.0), Point2::new(0.0, MAP_HEIGHT)).restitution(INIT_RESTITUTION));
 	// commands.spawn((
@@ -60,7 +66,7 @@ pub fn setup(commands: &mut Commands, mut configuration: ResMut<RapierConfigurat
 	// 	ColliderBuilder::segment(Point2::new(0.0, 0.0), Point2::new(0.0, MAP_HEIGHT))
 	// 		.restitution(INIT_RESTITUTION),
 	// ));
-	create_entity(commands, Role::Boundary("Right".to_string()),
+	create_entity(commands, Role::Boundary("Right".to_string()), 0.0, 0.0,
 				  RigidBodyBuilder::new_static(),
 				  ColliderBuilder::segment(Point2::new(MAP_WIDTH, 0.0), Point2::new(MAP_WIDTH, MAP_HEIGHT)).restitution(INIT_RESTITUTION));
 	// commands.spawn((
@@ -69,7 +75,7 @@ pub fn setup(commands: &mut Commands, mut configuration: ResMut<RapierConfigurat
 	// 	ColliderBuilder::segment(Point2::new(MAP_WIDTH, 0.0), Point2::new(MAP_WIDTH, MAP_HEIGHT))
 	// 		.restitution(INIT_RESTITUTION),
 	// ));
-	create_entity(commands, Role::Boundary("Bottom".to_string()),
+	create_entity(commands, Role::Boundary("Bottom".to_string()), 0.0, 0.0,
 				  RigidBodyBuilder::new_static(),
 				  ColliderBuilder::segment(Point2::new(0.0, MAP_HEIGHT), Point2::new(MAP_WIDTH, MAP_HEIGHT)).restitution(INIT_RESTITUTION));
 	// commands.spawn((
@@ -80,13 +86,12 @@ pub fn setup(commands: &mut Commands, mut configuration: ResMut<RapierConfigurat
 	// ));
 
 	// Random stuffs.
-	for _ in 0..1000 {
-		let x = rng.gen_range(20.0..8000.0);
-		let y = rng.gen_range(20.0..8000.0);
+	for _ in 0..100 {
+		let x = rng.gen_range(0.4 * MAP_WIDTH .. 0.6 * MAP_WIDTH);
+		let y = rng.gen_range(0.4 * MAP_HEIGHT .. 0.6 * MAP_HEIGHT);
 		let body = RigidBodyBuilder::new_dynamic().translation(x, y).mass(INIT_MASS, false);
 		let collider = ColliderBuilder::ball(INIT_RADIUS).restitution(INIT_RESTITUTION);
-		let entity = create_entity(commands, Role::Shape(0), body, collider);
-		commands.insert(entity, (Thrust { x: 0.0, y: 0.0 }, Transform::from_translation(Vec3::new(x, y, 0.0))));
+		create_entity(commands, Role::Shape(0), x, y, body, collider);
 		// commands.spawn((
 		// 	Shape { id: 0 },
 		// 	Thrust { x: 0.0, y: 0.0 },
@@ -96,10 +101,18 @@ pub fn setup(commands: &mut Commands, mut configuration: ResMut<RapierConfigurat
 		// ));
 	}
 
-	let planet = create_entity(commands, Role::CelestialBody("Planet".to_string()),
-							   RigidBodyBuilder::new_static().translation(2000.0, 2000.0).mass(CELESTIAL_MASS, false),
-							   ColliderBuilder::ball(CELESTIAL_RADIUS).restitution(INIT_RESTITUTION));
-	commands.insert_one(planet, Transform::identity());
+	create_entity(commands, Role::CelestialBody("Planet".to_string()),
+				  4029.99564, 5243.08753,
+				  RigidBodyBuilder::new_dynamic().mass(CELESTIAL_MASS, false).linvel(466.2036850, 432.3657300),
+				  ColliderBuilder::ball(CELESTIAL_RADIUS).restitution(INIT_RESTITUTION));
+	create_entity(commands, Role::CelestialBody("Planet".to_string()),
+				  5000.0, 5000.0,
+				  RigidBodyBuilder::new_dynamic().mass(CELESTIAL_MASS, false).linvel(-932.40737, -864.73146),
+				  ColliderBuilder::ball(CELESTIAL_RADIUS).restitution(INIT_RESTITUTION));
+	create_entity(commands, Role::CelestialBody("Planet".to_string()),
+				  5970.00436, 4756.91247,
+				  RigidBodyBuilder::new_dynamic().mass(CELESTIAL_MASS, false).linvel(466.2036850, 432.3657300),
+				  ColliderBuilder::ball(CELESTIAL_RADIUS).restitution(INIT_RESTITUTION));
 	// commands.spawn((
 	// 	CelestialBody { form: "".to_string() },
 	// 	Transform::identity(),
@@ -115,12 +128,11 @@ pub fn create_player(
 ) {
 	let mut rng = rand::thread_rng();
 	for event in events.drain() {
-		let x = rng.gen_range(500.0..3000.0);
-		let y = rng.gen_range(500.0..3000.0);
+		let x = rng.gen_range(0.4 * MAP_WIDTH .. 0.6 * MAP_WIDTH);
+		let y = rng.gen_range(0.4 * MAP_HEIGHT .. 0.6 * MAP_HEIGHT);
 		let body = RigidBodyBuilder::new_dynamic().translation(x, y).mass(INIT_MASS * 100.0, false);
 		let collider = ColliderBuilder::ball(INIT_RADIUS).restitution(INIT_RESTITUTION);
-		let entity = create_entity(commands, Role::Player(event.name.clone()), body, collider);
-		commands.insert(entity, (Thrust { x: 0.0, y: 0.0 }, Transform::from_translation(Vec3::new(x, y, 0.0))));
+		let entity = create_entity(commands, Role::Player(event.name.clone()), x, y,body, collider);
 		// commands.spawn((
 		// 	Player { name: event.name.clone() },
 		// 	Thrust { x: 0.0, y: 0.0 },
@@ -139,8 +151,8 @@ impl Command for ChangeMovement {
 	fn write(self: Box<Self>, world: &mut World, _resources: &mut Resources) {
 		let (fy, fx) = self.state.dir.map_or((0.0, 0.0), |dir| dir.sin_cos());
 		let mut thrust = world.get_mut::<Thrust>(self.player).expect("No component found.");
-		thrust.x = fx * 40000.0;
-		thrust.y = fy * 40000.0;
+		thrust.x = fx * 1000.0;
+		thrust.y = fy * 1000.0;
 		// let mut ori = world.get_mut::<Ori>(self.player).expect("No component found.");
 		// ori.deg = self.state.ori;
 	}
@@ -171,29 +183,32 @@ pub fn next_frame(
 	// player_query: Query<(&Thrust, &RigidBodyHandleComponent, &Transform), With<Player>>,
 	object_query: Query<
 		(&Thrust, &RigidBodyHandleComponent, &Transform),
-		Or<(With<Player>, With<Shape>)>,
+		Or<(With<Player>, With<Shape>, With<CelestialBody>)>,
 	>,
 ) {
-	for (thrust, player_handle, player_transform) in object_query.iter() {
+	for (thrust, object_handle, object_transform) in object_query.iter() {
 		let mut force = Vector2::new(thrust.x, thrust.y);
-		let player_body = rigid_body_set.get(player_handle.handle()).unwrap();
-		let player_mass = player_body.mass();
+		let object_body = rigid_body_set.get(object_handle.handle()).unwrap();
+		let object_mass = object_body.mass();
 		for (_, celestial_handle, celestial_transform) in celestial_query.iter() {
 			let celestial_mass = rigid_body_set.get(celestial_handle.handle()).unwrap().mass();
-			let displacement_3d = celestial_transform.translation - player_transform.translation;
+			let displacement_3d = celestial_transform.translation - object_transform.translation;
 			let displacement: Vector2<f32> = Vector2::new(displacement_3d.x, displacement_3d.y);
-			force += GRAVITY_CONST * player_mass * celestial_mass * displacement
+			if displacement.norm() == 0.0 {
+				continue;
+			}
+			force += GRAVITY_CONST * object_mass * celestial_mass * displacement
 				/ displacement.norm().powi(3);
 		}
-		rigid_body_set.get_mut(player_handle.handle()).unwrap().apply_force(force, true);
+		rigid_body_set.get_mut(object_handle.handle()).unwrap().apply_force(force, true);
 	}
 }
 
 pub fn collisions(events: ResMut<EventQueue>, collider_set: Res<ColliderSet>, mut rigid_body_set: ResMut<RigidBodySet>) {
 	while let Ok(contact_event) = events.contact_events.pop() {
 		if let ContactEvent::Started(first_handle, second_handle) = contact_event {
-			let mut first_body = rigid_body_set.get_mut(collider_set.get(first_handle).unwrap().parent()).unwrap();
-			let mut second_body = rigid_body_set.get_mut( collider_set.get(second_handle).unwrap().parent()).unwrap();
+			let first_body = rigid_body_set.get(collider_set.get(first_handle).unwrap().parent()).unwrap();
+			let second_body = rigid_body_set.get( collider_set.get(second_handle).unwrap().parent()).unwrap();
 			let mut first_entity = Entity::from_bits(first_body.user_data as u64);
 			let mut second_entity = Entity::from_bits(second_body.user_data as u64);
 		}
